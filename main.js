b723e57c1976ae796c689fb7d7ddd173b43dc7b6
shab20 <shab_93@hotmail.com>
2023-01-25 22:28:54 +0400

    Done

---
+++ main.js
@@ -0,0 +1,26 @@
+// 1
+
+let sentence = "The movie is not that bad, i like it";
+
+console.log(sentence);
+
+
+//2
+let wordNot = sentence.indexOf("not");
+console.log(wordNot);
+
+
+//3
+
+let wordBad =sentence.indexOf("bad");
+console.log(wordBad);
+
+//4
+
+    console.log(sentence.replace("not that bad", "good"));
+
+
+
+//5
+
+console.log(sentence);
